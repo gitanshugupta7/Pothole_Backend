4cf4826ca5073d01ae1beffb79080b2051388052
Gitanshu <gitanshugupta7@gmail.com>
2020-07-31 11:36:34 +0530

    updated code

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o!eri9959^mxlikk7q@wb2lxdd6c_+^^#2*u#6xc8gf$it84+z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','dac821be40de.ngrok.io']
 
 
 CORS_ORIGIN_ALLOW_ALL = True
